c2fe45dc88b0128ae47d595a0c843be0b9d1ae34
Davoleo <dav@davoleo.net>
2024-05-22 13:57:59 +0200

    Some Metals - NICKEL - CHROMIUM - TITANIUM - WOLFRAM - BRONZE - INVAR - ELINVAR

--- common/src/main/java/net/davoleo/mettle/init/CoreMetals.java
+++ common/src/main/java/net/davoleo/mettle/init/CoreMetals.java
@@ -15,30 +15,83 @@ public class CoreMetals implements IMettleIntegration {
 
     public static final IMetal TIN = SimpleMetal
             .wizard("tin")
-            .enchantability(2)
-            .toolStats(it -> it
-                    .attackSpeed(4)
-                    .harvestLevel(4)
-                    .damage(1)
-                    .efficiency(1)
-            )
-            .armorStats(b -> b
-                    .movementSpeed(2)
-                    .maxHealth(4)
-                    .knockbackResistance(2)
-                    .damageReduction(1,1,1,1)
-            )
+            .color(0xBEBBAF)
             .oreVariants(OreVariant.STONE, OreVariant.DEEPSLATE)
             .component(ComponentType.ORE)
             .build();
 
+    public static final IMetal NICKEL = SimpleMetal
+            .wizard("nickel")
+            .oreVariants(OreVariant.STONE, OreVariant.DEEPSLATE)
+            .component(ComponentType.ORE)
+            .build();
+
+    public static final IMetal CHROMIUM = SimpleMetal
+            .wizard("chromium")
+            .oreVariants(OreVariant.DEEPSLATE, OreVariant.END_STONE)
+            .component(ComponentType.ORE)
+            .build();
+
+    public static final IMetal TITANIUM = SimpleMetal
+            .wizard("titanium")
+            .color(0xFFFFFF)
+            .oreVariants(OreVariant.DEEPSLATE)
+            .component(ComponentType.ORE)
+            .build();
+
+    public static final IMetal WOLFRAM = SimpleMetal
+            .wizard("wolfram")
+            .oreVariants(OreVariant.NETHERRACK)
+            .component(ComponentType.ORE)
+            .build();
+
+
+    //Alloys
+    public static final IMetal BRONZE = SimpleMetal
+            .wizard("bronze")
+            .build();
+    public static final IMetal INVAR = SimpleMetal
+            .wizard("invar")
+            .build();
+    public static final IMetal ELINVAR = SimpleMetal
+            .wizard("elinvar")
+            .build();
+
     @Override
     public void registerMetals(Registry<IMetal> registry) {
-        registry.register(TIN.name(), TIN);
+        registry.register(TIN);
+        registry.register(NICKEL);
+        registry.register(CHROMIUM);
+        registry.register(TITANIUM);
+        registry.register(WOLFRAM);
+
+        registry.register(BRONZE);
+        registry.register(INVAR);
+        registry.register(ELINVAR);
     }
 
     @Override
     public void registerAlloys(Registry<Alloy> registry) {
+        registry.register(Alloy
+                .create(BRONZE)
+                .addMetal(VanillaMetals.COPPER, 3)
+                .addMetal(TIN, 1)
+                .build()
+        );
+
+        registry.register(Alloy
+                .create(INVAR)
+                .addMetal(VanillaMetals.IRON, 2)
+                .addMetal(NICKEL, 1)
+                .build()
+        );
+
+        registry.register(Alloy
+                .create(ELINVAR)
+                .addMetal(INVAR, 9)
+                .addMetal(CHROMIUM, 1)
+                .build()
+        );
 
     }
 }
