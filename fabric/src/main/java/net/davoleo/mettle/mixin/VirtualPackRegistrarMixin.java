e7ec907b2ca61fd3f71345c2048b63149ed63ad7
Davoleo <dav@davoleo.net>
2024-05-25 16:49:53 +0200

    improve VirtualPackRegistration Mixin compat
    Co-authored-by: PierKnight <45203064+pierknight@users.noreply.github.com>

--- fabric/src/main/java/net/davoleo/mettle/mixin/VirtualPackRegistrarMixin.java
+++ fabric/src/main/java/net/davoleo/mettle/mixin/VirtualPackRegistrarMixin.java
@@ -2,19 +2,33 @@ package net.davoleo.mettle.mixin;
 
 import com.google.common.collect.ImmutableSet;
 import net.davoleo.mettle.data.VirtualRepositorySource;
+import net.minecraft.server.packs.repository.Pack;
 import net.minecraft.server.packs.repository.PackRepository;
+import net.minecraft.server.packs.repository.RepositorySource;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Set;
 
 @Mixin(PackRepository.class)
 public class VirtualPackRegistrarMixin {
 
-    @Redirect(
+    @Final @Shadow @Mutable
+    private Set<RepositorySource> sources;
+
+    @Inject(
             method = "<init>(Lnet/minecraft/server/packs/repository/Pack$PackConstructor;[Lnet/minecraft/server/packs/repository/RepositorySource;)V",
-            at = @At(value = "INVOKE", target = "Lcom/google/common/collect/ImmutableSet;copyOf([Ljava/lang/Object;)Lcom/google/common/collect/ImmutableSet;")
+            at = @At("TAIL")
     )
-    private ImmutableSet<Object> onAssignSources(Object[] elements) {
-        return ImmutableSet.builder().add(elements).add(VirtualRepositorySource.get()).build();
+    private void onConstruction(Pack.PackConstructor constructor, RepositorySource[] sources, CallbackInfo ci) {
+        this.sources = ImmutableSet.<RepositorySource>builder()
+                .add(sources)
+                .add(VirtualRepositorySource.get())
+                .build();
     }
 }
